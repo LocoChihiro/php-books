0614616d3e552c6eb2919b58d64ef8891bfb6e13
UBANTU <hansai1026@outlook.com>
2019-03-26 03:10:33 -0700

    AE API frist

---
+++ models/Books.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "books".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $author
+ * @property double $price
+ */
+class Books extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'books';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'author', 'price'], 'required'],
+            [['name', 'author'], 'string'],
+            [['price'], 'number'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'author' => 'Author',
+            'price' => 'Price',
+        ];
+    }
+}
